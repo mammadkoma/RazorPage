b27b05554d3c1953b56f74a876558e34f9eb8eec
Mohammad Komaei <komaei@live.com>
2022-05-15 16:22:06 +0430

    first

---
+++ RazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<object> tests { get; set; }
+
+        public void OnGet()
+        {
+            var service = new TestService();
+            tests = service.GetAll();
+        }
+    }
+
+    public class TestService
+    {
+        public List<object> GetAll()
+        {
+            return new List<object>() { new { Id = 1, Name = "111" }, new { Id = 2, Name = "222" } };
+        }
+    }
+
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RazorPage/Pages/Index.cshtml.js
@@ -0,0 +1,5 @@
+﻿function btnClick(id) {
+    let aa = 5;
+    if (aa >= 5)
+        alert('clicked..');
+}
